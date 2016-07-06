e5ac09540dceaacfd83d7706bc8dc1ca01fad781
pprakash7 <pratikprakashis@gmail.com>
2016-07-05 20:28:10 -0400

    parse for video id

--- playVideo.php
+++ playVideo.php
@@ -4,7 +4,8 @@ require_once "suppor.php";
 require_once "databaseAccessObject.php";
 session_start();
 $video = trim($_POST['video']);
-$src = "https://www.youtube.com/embed/".$video;
+$vidId = substr($video, -11);
+$src = "https://www.youtube.com/embed/".$vidId;
 $body = '<iframe width="960" height="540" align="middle" src="'.$src.'" frameborder="0" allowfullscreen></iframe><br><br>';
 $body .= '<form action="videoTable.php">
                             <input type="submit" value="Back to videos"/>
